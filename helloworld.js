98f2b74ac5a7d96797275e4a4ff66bb5fa0d3fc8
Mukunda9000 <mukunda9000@gmail.com>
2019-07-22 14:24:39 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -9,7 +9,7 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
    response.end('Hello Devops!\n')
-}).listen(22)
+}).listen(3000)
 
 // Console will print the message
 console.log('Server running')
